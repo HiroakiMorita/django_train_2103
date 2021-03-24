5825949e9201f1c23b19d88a224feb1c288f2484
HiroakiMorita <taiyou3291@outlook.jp>
2021-03-25 02:22:33 +0900

    first commit

--- django_train_2103/settings.py
+++ django_train_2103/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!q=0fub)f4d*-(+h8myv5qzr$cm^)z3am!)4t9dbbq)=+t##ub'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
